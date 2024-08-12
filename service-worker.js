fff8905824a650bd28cdaaaa0dca7682eb11431b
Paulo Henrique Aleixo de Campos <119341826+phaleixo@users.noreply.github.com>
2024-08-12 13:10:10 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,8 +6,8 @@ self.addEventListener('install', (event) => {
         '/index.html',
         '/style.css',
         '/scripts.js',
-        '/icon-192x192.png',
-        '/icon-512x512.png'
+        '/icons/icon-192x192.png',
+        '/icons/icon-512x512.png'
       ]);
     })
   );
