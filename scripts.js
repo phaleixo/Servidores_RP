8bfc9518418a78e458c64812a959279feaef8a4f
phaleixo <phaleixo1@gmail.com>
2024-08-14 18:31:46 -0300

    deleted:    scripts.js 	modified:   service-worker.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw.js').then(function(registration) {
-        console.log('Service Worker registrado com sucesso:', registration.scope);
-      }, function(error) {
-        console.log('Falha ao registrar o Service Worker:', error);
-      });
-    });
-  }
